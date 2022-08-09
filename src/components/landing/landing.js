6dc3eca6d7ebdcf552a44b462b40eca659ab0458
Mohit20Kumar <mohitkumar20021220@gmail.com>
2022-08-09 16:00:55 +0530

    nav links

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -5,21 +5,19 @@ import Typewriter from "typewriter-effect";
 export default class Landing extends Component {
   render() {
     return (
-      <>
-        <div id="hero" className='landing-main'>
-          <ParticleBackground />
-          <div className='hvr-shutter-in-vertical name'>REVA Hack</div>
-          <div className='name-2'>
-            <Typewriter
-              options={{
-                strings: ["Coming Soon..", "The Hack Is Back.."],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </div>
-          {/* After Coming soon gets over */}
-          {/* <div className='main-buttons'>
+      <div id="hero" className="landing-main">
+        <div className="hvr-shutter-in-vertical name">REVA Hack</div>
+        <div className="name-2">
+          <Typewriter
+            options={{
+              strings: ["Coming Soon..", "The Hack Is Back.."],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        {/* After Coming soon gets over */}
+        {/* <div className='main-buttons'>
           <button>Button 1</button>
           <button>Button 2</button>
         </div> */}
