7117620ff110b897be7bf42499ac2c4ae18abf73
charana555 <charanchandrashekar55@gamil.com>
2022-08-18 22:28:11 +0530

    Added tag line for the tracks

---
+++ src/data/tracksData.js
@@ -0,0 +1,39 @@
+import aiicon from "../images/ai_icon.svg";
+import blicon from "../images/blockchain_icon.svg";
+import vricon from "../images/virtual_reality.svg";
+import ccicon from "../images/cloud_computing.svg";
+import ioticon from "../images/iot_icon.svg";
+import hlicon from "../images/health_icon.svg";
+
+export const trackData = [
+  {
+    title: "AI / ML",
+    paragraph: "Say Hello to the Future",
+    image: aiicon,
+  },
+  {
+    title: "AR / VR",
+    paragraph: "Get transported to new worlds through VR",
+    image: vricon,
+  },
+  {
+    title: "Block Chain",
+    paragraph: "Building Blocks for a Better World",
+    image: blicon,
+  },
+  {
+    title: "Cloud Computing",
+    paragraph: "See you on the cloud ",
+    image: ccicon,
+  },
+  {
+    title: "Internet Of Things",
+    paragraph: "A home for things",
+    image: ioticon,
+  },
+  {
+    title: "Health Care",
+    paragraph: "For the Health of It",
+    image: hlicon,
+  },
+];
