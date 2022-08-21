03f14ffb9d7a22ea00c93ea02fc6d8fa9825b377
Vishesh-Krishna-S-A <97118248+Vishesh-Krishna-S-A@users.noreply.github.com>
2022-08-21 20:46:45 +0530

    timeline

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Landing from "./components/landing/landing.js";
 import About from "./components/about/about.js";
 import Tracks from "./components/tracks/tracks.js";
 import Prizes from "./components/prizes/prizes.js";
+import Timeline from "./components/timeline/timeline.js";
 import Sponsors from "./components/sponsors/sponsors.js";
 import FAQ from "./components/faq/faq.js";
 import Footer from "./components/footer/footer.js";
+
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
       <About />
       <Tracks />
       <Prizes />
+      <Timeline />
       <Sponsors />
       <FAQ />
       <Footer />
